25b3716bc033a5a0917ed98db374fcb4b1da2c8d
SosthenesMS <sosthenesms@yahoo.com.br>
2022-03-24 19:45:32 -0300

    update

--- Iniciante/1012.java
+++ Iniciante/1012.java
@@ -1,36 +1,28 @@
-import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+ 
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+ 
+    public static void main(String[] args) throws IOException {
         
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+       Scanner in = new Scanner(System.in);
         
-        double A, B, C, resultado;
-        A = Double.parseDouble(in.readLine());
-        B = Double.parseDouble(in.readLine());
-        C = Double.parseDouble(in.readLine());
-      
-        //Area do triangulo
-        resultado = ((A*C)/2);
+        float A = in.nextFloat();
+        float B = in.nextFloat();
+        float C = in.nextFloat();
+        float resultado = ((A*C)/2);
         System.out.printf("TRIANGULO: %.3f\n",resultado);
+        float resultado2 = (float) ((3.14159)*(Math.pow(C, 2)));
+        System.out.printf("CIRCULO: %.3f\n",resultado2);
+        float resultado3 = (((A+B)/2)*C);
+        System.out.printf("TRAPEZIO: %.3f\n",resultado3);
+        float resultado4 = (float) Math.pow(B, 2);
+        System.out.printf("QUADRADO: %.3f\n", resultado4);
+        float resultado5 = A*B;
+        System.out.printf("RETANGULO: %.3f\n", resultado5);
         
-        //Area do circulo
-        resultado = ((3.14159)*(Math.pow(C, 2)));
-        System.out.printf("CIRCULO: %.3f\n",resultado);
-        
-       //Area do trapezio
-       resultado = (((A+B)/2)*C);
-        System.out.printf("TRAPEZIO: %.3f\n",resultado);
-        
-        //Area do qadrado
-        resultado = Math.pow(B, 2);
-        System.out.printf("QUADRADO: %.3f\n", resultado);
-        
-        //Area do retangulo
-        resultado = A*B;
-        System.out.printf("RETANGULO: %.3f\n", resultado);
-        
+ 
     }
-    
+ 
 }
-
